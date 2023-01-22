2a9b3e4c9a1b58e44d0be35c249cab81b5fff5c2
AxelSiwmark1 <siwmark@net.chalmers.se>
2023-01-22 12:59:00 +0100

    Update gui.py
    Can generate starting grid with numbers

--- gui.py
+++ gui.py
@@ -1,8 +1,13 @@
 import numpy as np
 import pygame
+import requests
+from sudoku import Sudoku
 
 WIDTH = 550
 background_color = (251, 247, 245)
+grid_element_colour = (50, 30, 150)
+puzzle = Sudoku(3).difficulty(0.5)
+grid = puzzle.board
 
 def main():
     pygame.init()
@@ -10,6 +15,7 @@ def main():
     pygame.display.set_caption("Sudoko")
     #set background colour
     win.fill(background_color)
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
     #Create grid
     for i in range(0,10):
         if(i%3 == 0):
@@ -18,6 +24,13 @@ def main():
             thickness = 2            
         pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), (50 + 50*i, 500), thickness) #Vertical lines
         pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), (500, 50 + 50*i), thickness) #Horizontal lines
+    pygame.display.update()
+
+    for r in range(0, len(grid[0])):
+        for c in range(0, len(grid[0])):
+            if grid[r][c] is not None:
+                value = myfont.render(str(grid[r][c]), True, grid_element_colour)
+                win.blit(value, ((c+1)*50 + 15,(r+1)*50 + 0))
     pygame.display.update()
 
 
