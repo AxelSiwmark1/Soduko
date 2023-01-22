c4b99338f5000357e25992249a2d8472bf0b6fac
AxelSiwmark1 <siwmark@net.chalmers.se>
2023-01-22 12:26:11 +0100

    Update gui.py
    Created the grid

--- gui.py
+++ gui.py
@@ -2,12 +2,20 @@ import numpy as np
 import pygame
 
 WIDTH = 550
-background_color = (250, 250, 250)
+background_color = (251, 247, 245)
 
 def main():
     pygame.init()
     win = pygame.display.set_mode((WIDTH, WIDTH))
     pygame.display.set_caption("Sudoko")
+    #set background colour
+    win.fill(background_color)
+    #Create grid
+    for i in range(0,10):
+        pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), (50 + 50*i, 500), 2) #Vertical lines
+        pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), (500, 50 + 50*i), 2) #Horizontal lines
+    pygame.display.update()
+
 
     while True:
         for event in pygame.event.get():
