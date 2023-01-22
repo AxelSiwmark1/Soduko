42522894518b4b34526b0464b7e285f0b9fbb857
AxelSiwmark1 <siwmark@net.chalmers.se>
2023-01-22 14:08:32 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -6,8 +6,39 @@ from sudoku import Sudoku
 WIDTH = 550
 background_color = (251, 247, 245)
 grid_element_colour = (50, 30, 150)
+buffer = 5
 puzzle = Sudoku(3).difficulty(0.5)
 grid = puzzle.board
+grid_orginal = [[grid[x][y] for y in range(len(grid[0]))] for x in range(len(grid))]
+
+def insert(win, position):
+    r,c = position[1], position[0] #flipped when working in the backend
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.KEYDOWN:
+                #1 tries to edit original grid
+                if grid[r-1][c-1] is not None:
+                    return
+                #2 edit, 48 = 0 ascii
+                if(event.key == 48):
+                    grid[r-1][c-1] == event.key - 48
+                    pygame.draw.rect(win, background_color, (position[0]* 50 + buffer, position[1]*50 + buffer, 50 - 2*buffer, 50 - 2*buffer))
+                    pygame.display.update()
+                #3 adding the digigts 
+                if(0 < event.key - 48 < 10):
+                    pygame.draw.rect(win, background_color, (position[0]* 50 + buffer, position[1]*50 + buffer, 50 - 2*buffer, 50 - 2*buffer))
+                    value = myfont.render(str(event.key - 48), True, (0,0,0))
+                    win.blit(value, (position[0]*50 + 15, position[1]*50))
+                    grid[r-1][c-1] = event.key - 48
+                    pygame.display.update()
+                    return
+                return
+
+    
 
 def main():
     pygame.init()
@@ -37,6 +68,9 @@ def main():
 
     while True:
         for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                pos = pygame.mouse.get_pos
+                insert(win, (pos[0]//50, pos[1]//50))
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
