abe5dc217242bc70413ab57650c2b635b1f7f908
AxelSiwmark1 <siwmark@net.chalmers.se>
2023-01-22 12:17:21 +0100

    Update gui.py
    GUI init

--- gui.py
+++ gui.py
@@ -1,1 +1,18 @@
+import numpy as np
 import pygame
+
+WIDTH = 550
+background_color = (250, 250, 250)
+
+def main():
+    pygame.init()
+    win = pygame.display.set_mode((WIDTH, WIDTH))
+    pygame.display.set_caption("Sudoko")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+main()
