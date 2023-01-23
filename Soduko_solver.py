e3816e10ec673f4202b1bec10c361441e84be4e2
AxelSiwmark1 <89916825+AxelSiwmark1@users.noreply.github.com>
2023-01-23 03:36:30 -0800

    it works

--- Soduko_solver.py
+++ Soduko_solver.py
@@ -1,16 +1,15 @@
 import numpy as np
+import pygame
 
-grid = [[5,3,0,0,7,0,0,0,0],
-        [6,0,0,1,9,5,0,0,0],
-        [0,9,8,0,0,0,0,6,0],
-        [8,0,0,0,6,0,0,0,3],
-        [4,0,0,8,0,3,0,0,1],
-        [7,0,0,0,2,0,0,0,6],
-        [0,6,0,0,0,0,2,8,0],
-        [0,0,0,4,1,9,0,0,5],
-        [0,0,0,0,8,0,0,0,0]]
+background_color = (251, 247, 245)
+grid_element_colour = (50, 30, 150)
+buffer = 5
 
-print(np.matrix(grid))
+
+
+grid_test = [[None, None, None, 9, None, 3, None, 5, 6], [4, None, None, 6, 8, None, 1, 2, None], [2, 6, None, None, None, 5, 3, None, None], [8, 9, None, None, 6, 4, 7, 3, 5], [None, None, None, 1, 7, None, 8, 4, 2], [None, None, None, 3, None, None, None, 6, None], [None, 2, 6, 8, None, 1, None, None, 7], [5, 1, 4, None, 9, 2, 6, None, 3], [9, 8, None, None, None, None, None, None, None]]
+
+#print(np.matrix(grid))
 
 
 
@@ -34,17 +33,33 @@ def possible_solution(row, col, number, grid):
                 return False
     return True
 
-def solve(grid):
+
+def solve(grid, win, solved):
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
+    solved = 0
     for row in range(0,9):
         for col in range(0,9):
-            if grid[row][col] == 0:
+            if grid[row][col] is None:
                 for number in range(1,10):
-                    if possible_solution(row, col, number, grid):
+                    if possible_solution(row, col, number, grid) and solved ==0:
                         grid[row][col] = number
-                        solve(grid)
-                        grid[row][col] = 0
+                        value = myfont.render(str(number), True, (0,0,0))
+                        win.blit(value, ((col+1)*50 + 15,(row+1)*50 + 0))
+                        pygame.display.update()
+                        pygame.time.delay(25)
+
+                        solve(grid,win, solved)
+                        if(solved == 1):
+                            return
+
+                        grid[row][col] = None
+                        pygame.draw.rect(win, background_color, ((col+1) * 50 + buffer, (row + 1)*50 + buffer, 50 - 2*buffer, 50 - 2*buffer))
+                        pygame.display.update()
                 return
-    print(np.matrix(grid))
+    solved = 1
+    
+    
+    
+    
 
 
-solve(grid)
--- gui.py
+++ gui.py
@@ -2,13 +2,73 @@ import numpy as np
 import pygame
 from sudoku import Sudoku
 
+
 WIDTH = 550
 background_color = (251, 247, 245)
 grid_element_colour = (50, 30, 150)
 buffer = 5
 puzzle = Sudoku(3).difficulty(0.5)
-grid = puzzle.board
-grid_orginal = [[grid[x][y] for y in range(len(grid[0]))] for x in range(len(grid))]
+grid_board = puzzle.board
+grid_orginal = [[grid_board[x][y] for y in range(len(grid_board[0]))] for x in range(len(grid_board))]
+
+
+
+def possible_solution(row, col, number, grid):
+
+    #is it in row?
+    for c in range(0,9):
+        if grid[row][c] == number:
+            return False
+    #is it in col?
+    for r in range(0,9):
+        if grid[r][col] == number:
+            return False
+    #is it in square?
+    square_row = 3 * (row // 3)
+    square_col = 3 * (col // 3)
+    for r in range(square_row, square_row + 3):
+        for c in range(square_col, square_col + 3):
+            if grid[r][c] == number:
+                return False
+    return True
+
+solved = 0
+def solve(grid, win):
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
+    for row in range(0,9):
+        for col in range(0,9):
+            if grid[row][col] is None:
+                for number in range(1,10):
+                    if possible_solution(row, col, number, grid):
+                        grid[row][col] = number
+                        value = myfont.render(str(number), True, (0,0,0))
+                        win.blit(value, ((col+1)*50 + 15,(row+1)*50 + 0))
+                        pygame.display.update()
+                        pygame.time.delay(25)
+
+                        solve(grid,win)
+                        global solved
+                        if(solved == 1):
+                            return
+
+                        grid[row][col] = None
+                        pygame.draw.rect(win, background_color, ((col+1) * 50 + buffer, (row + 1)*50 + buffer, 50 - 2*buffer, 50 - 2*buffer))
+                        pygame.display.update()
+                return
+    solved = 1
+    pass
+
+def draw(win, grid):
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
+    for r in range(0, 9):
+        for c in range(0, 9):
+            if grid[r][c] is not None:
+                value = myfont.render(str(grid[r][c]), True, grid_element_colour)
+                win.blit(value, ((c+1)*50 + 15,(r+1)*50 + 0))
+    pygame.display.update()
+    return
+
+
 
 def insert(win, position):
     r,c = position[1], position[0] #flipped when working in the backend
@@ -24,7 +84,7 @@ def insert(win, position):
                     return
                 #2 edit, 48 = 0 ascii
                 if(event.key == 48):
-                    grid[r-1][c-1] == event.key - 48
+                    grid_board[r-1][c-1] == event.key - 48
                     pygame.draw.rect(win, background_color, (position[0]* 50 + buffer, position[1]*50 + buffer, 50 - 2*buffer, 50 - 2*buffer))
                     pygame.display.update()
                 #3 adding the digigts 
@@ -32,9 +92,12 @@ def insert(win, position):
                     pygame.draw.rect(win, background_color, (position[0]* 50 + buffer, position[1]*50 + buffer, 50 - 2*buffer, 50 - 2*buffer))
                     value = myfont.render(str(event.key - 48), True, (0,0,0))
                     win.blit(value, (position[0]*50 + 15, position[1]*50))
-                    grid[r-1][c-1] = event.key - 48
+                    grid_board[r-1][c-1] = event.key - 48
                     pygame.display.update()
                     return
+                if(event.key == 115):# if s is pressed: solve
+                    solve(grid_orginal, win)
+
                 return
 
     
@@ -57,12 +120,7 @@ def main():
     pygame.display.update()
 
     #Create number for starting grid
-    for r in range(0, len(grid[0])):
-        for c in range(0, len(grid[0])):
-            if grid[r][c] is not None:
-                value = myfont.render(str(grid[r][c]), True, grid_element_colour)
-                win.blit(value, ((c+1)*50 + 15,(r+1)*50 + 0))
-    pygame.display.update()
+    draw(win,grid_board)
 
 
     while True:
